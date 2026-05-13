c0e0a27963d4c49e115666cf501ec9b4b6884656
nagayoshiyoshie <nagayoshiyoshie@192.168.0.80>
2026-05-13 21:19:18 +0900

    question4

---
+++ HelloWorld_Nagayoshimie/src/curriculum/b/Question4.java
@@ -0,0 +1,21 @@
+package curriculum.b;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Question1
+		int[] scores = {1, 2, 3, 4, 5};
+		for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+		//Question2
+		int[] numbers = {10, 20, 30, 40, 50};
+		for (int i = numbers.length - 1; i>=0; i--) {
+            System.out.println(numbers[i]);
+        }
+        }
+
+	}
+
+

1d00047b9bcc49f14e11775b1b47286c96cecd0e
nagayoshiyoshie <nagayoshiyoshie@192.168.0.80>
2026-05-20 13:58:20 +0900

    Question5

---
+++ HelloWorld_Nagayoshimie/src/curriculum/b/Animal.java
@@ -0,0 +1,34 @@
+package curriculum.b;
+
+public class Animal {
+
+	//public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		private String name;
+	    private double length;
+	    private int speed;
+	    
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	    public String getName() {
+	        return this.name;
+	    }
+	    
+	    public void setLength(double length) {
+	        this.length = length;
+	    }
+	    public double getLength() {
+	        return this.length;
+	    }
+	    
+	    public void setSpeed(int speed) {
+	        this.speed = speed;
+	    }
+	    public int getSpeed() {
+	        return this.speed;
+	    
+
+	}
+
+}
---
+++ HelloWorld_Nagayoshimie/src/curriculum/b/Greeting.java
@@ -0,0 +1,14 @@
+package curriculum.b;
+
+public class Greeting {
+
+	//public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		public void sayHello() {
+	        System.out.println("こんにちは！");
+	    }
+	
+
+	}
+
+

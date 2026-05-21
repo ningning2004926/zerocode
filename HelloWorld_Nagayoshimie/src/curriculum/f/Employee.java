0609f529533cf4fb6c891d65d190ff6abe3de73f
nagayoshiyoshie <nagayoshiyoshie@192.168.0.80>
2026-05-21 13:57:00 +0900

    22.2

---
+++ HelloWorld_Nagayoshimie/src/curriculum/f/Employee.java
@@ -0,0 +1,15 @@
+package curriculum.f;
+
+public class Employee {
+
+	//public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		public String employeeId;
+		public String name;
+		public void showlnfo() {
+			System.out.println("社員ID: " + this.employeeId + ", 名前: " + this.name);
+	    }
+
+	}
+
+
---
+++ HelloWorld_Nagayoshimie/src/curriculum/f/Main.java
@@ -0,0 +1,20 @@
+package curriculum.f;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Employee emp = new Employee();
+
+        
+        emp.name = "佐藤太郎";
+
+        
+        emp.employeeId = "E001";
+        
+        emp.showlnfo();
+    }
+
+	}
+
+

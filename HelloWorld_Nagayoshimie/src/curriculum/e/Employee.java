f274f67e171fd07da1f73ec3494f7eb343733935
nagayoshiyoshie <nagayoshiyoshie@192.168.0.80>
2026-05-21 13:40:28 +0900

    22.1

---
+++ HelloWorld_Nagayoshimie/src/curriculum/e/Employee.java
@@ -0,0 +1,14 @@
+package curriculum.e;
+
+public class Employee {
+
+	//public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		public String name;
+		public void work() {
+	        System.out.println(this.name + "は働いています。");
+	    }
+
+	}
+
+
---
+++ HelloWorld_Nagayoshimie/src/curriculum/e/Main.java
@@ -0,0 +1,18 @@
+package curriculum.e;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Employee emp = new Employee();
+
+        // name に「山田太郎」を設定する
+        emp.name = "山田太郎";
+
+        // work() を呼び出して出力を確認する
+        emp.work();
+    }
+
+	}
+
+
